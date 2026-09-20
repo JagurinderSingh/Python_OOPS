e8d054e98dfa9527e52c3e602ace3a20ea6f88f7
JagurinderSingh <jagurindersingh02@gmail.com>
2026-09-20 14:22:24 +0530

    object and class notes

---
+++ Lecture_1/Class_and_Object.py
@@ -0,0 +1,27 @@
+L = [1, 2, 3, 4, 5]
+print(type(L)) # <class 'list'>
+
+# L.upper() # No upper function in the list
+# AttributeError: 'list' object has no attribute 'upper'
+
+# All datatypes we studied like list, string, etc are the built in classes.
+
+# When we develop a new variable under a certain datatype, we are creating an object of that class.
+
+# Class is a blueprint for creating objects. It defines a set of attributes and methods that the created objects will have.
+
+s = "hello"
+s = [1, 2, 3]
+
+# Class has two things - Data or property and functions or behaviour. e.g. Car is the class and it has properties like color, model, etc and behaviour like avg_speed, etc.
+
+# Object is an instance of a class.
+
+# syntax to create an object
+#object_name = class_name()
+
+L = list() # Creating an object of list class
+
+s = str() # Creating an object of string class
+
+
