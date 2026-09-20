4c8ee0b86a5da543f2f6dc4d0175bab73c9eae3c
JagurinderSingh <jagurindersingh02@gmail.com>
2026-09-20 13:25:54 +0530

    First few lines

---
+++ Lecture_1/OOP_1.py
@@ -0,0 +1,13 @@
+# L = [1, 2, 3]
+
+# L.upper() #No upper function in the list
+
+s = "hello"
+s.append('x') #No append function in the string
+
+# Python is calling list and string, even any other thing is object.
+
+# "Everything in the Python is an object."
+
+
+
