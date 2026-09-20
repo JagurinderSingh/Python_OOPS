034c3c56358616465b32270b9c533aa7422b0fc9
JagurinderSingh <jagurindersingh02@gmail.com>
2026-09-20 14:09:20 +0530

    more notes

--- Lecture_1/OOP_1.py
+++ Lecture_1/OOP_1.py
@@ -9,5 +9,8 @@ s.append('x') #No append function in the string
 
 # "Everything in the Python is an object."
 
+# The main power of the OOP is that it gives power to the programmer to create their own objects or datatypes which are specific to the application they are trying to build.
+
+#Procedural programming is very basic while the OOP is more advanced and powerful, used for large scale applications.
 
 
